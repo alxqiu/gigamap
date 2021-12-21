fd1e050d171f5512576f46d5a7822445dd453c01
Alexander Qiu <40442780+alxqiu@users.noreply.github.com>
2021-12-20 16:17:17 -0800

    added info on the port

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../build'))
 })
 
+// apis available on localhost : 5000
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
